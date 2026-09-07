81850e59318285e7ab341511af99b44601a88a7d
mktran76 <tranminhkhoi@fabercompany.co.jp>
2026-09-07 11:34:11 +0700

    Update dummy API

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from 'next';
 import { Be_Vietnam_Pro, IBM_Plex_Mono, Newsreader } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 import AtlasShell from '@/components/AtlasShell';
 import { CatalogProvider } from '@/components/CatalogProvider';
@@ -55,6 +56,35 @@ export default async function RootLayout({ children }: { children: React.ReactNo
       className={`${display.variable} ${ui.variable} ${mono.variable}`}
       suppressHydrationWarning
     >
+      <head>
+        {/* Mieruca Embed Code */}
+        <Script
+          id="mierucajs"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+window.__fid = window.__fid || [];__fid.push([149456490]);
+(function() {
+function mieruca(){if(typeof window.__fjsld != "undefined") return; window.__fjsld = 1; var fjs = document.createElement('script'); fjs.type = 'text/javascript'; fjs.async = true; fjs.id = "fjssync"; var timestamp = new Date;fjs.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://hm.mieru-ca.com/service/js/mieruca-hm.js?v='+ timestamp.getTime(); var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(fjs, x); };
+setTimeout(mieruca, 500); document.readyState != "complete" ? (window.attachEvent ? window.attachEvent("onload", mieruca) : window.addEventListener("load", mieruca, false)) : mieruca();
+})();
+`,
+          }}
+        />
+        {/* Mieruca Optimize Tag */}
+        <Script
+          id="mierucaOptimizejs"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+window.__optimizeid = window.__optimizeid || [];__optimizeid.push([1428893104]);
+(function () {var fjs = document.createElement('script');fjs.type = 'text/javascript';
+fjs.async = true;fjs.id = "fjssync";var timestamp = new Date;fjs.src = 'https://opt.mieru-ca.com/service/js/mieruca-optimize.js?v=' + timestamp.getTime();
+var x = document.getElementsByTagName('script')[0];x.parentNode.insertBefore(fjs, x);})();
+`,
+          }}
+        />
+      </head>
       <body>
         {/* Áp theme đã lưu trước khi React hydrate, để không nháy sáng rồi mới đổi màu. */}
         <script
