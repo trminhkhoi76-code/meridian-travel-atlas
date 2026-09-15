59736e9504cade9cb41b393411d68ccc1317ee88
mktran76 <tranminhkhoi@fabercompany.co.jp>
2026-09-15 15:33:16 +0700

    Revert "Update layout.tsx"
    This reverts commit 1b84f14f8c45aa3eeefebc19df2453c7e592e6e9.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,7 +63,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-window.__fid = window.__fid || [];__fid.push([392109481]);
+window.__fid = window.__fid || [];__fid.push([149456490]);
 (function() {
 function mieruca(){if(typeof window.__fjsld != "undefined") return; window.__fjsld = 1; var fjs = document.createElement('script'); fjs.type = 'text/javascript'; fjs.async = true; fjs.id = "fjssync"; var timestamp = new Date;fjs.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://hm.mieru-ca.com/service/js/mieruca-hm.js?v='+ timestamp.getTime(); var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(fjs, x); };
 setTimeout(mieruca, 500); document.readyState != "complete" ? (window.attachEvent ? window.attachEvent("onload", mieruca) : window.addEventListener("load", mieruca, false)) : mieruca();
